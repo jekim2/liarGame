d8a0962d6345d057e1c2ed0631b51378869d3072
jekim2 <bluesara44@gmail.com>
2020-03-03 18:05:30 +0900

    movie api (2)

--- liar-game/src/pages/SettingGames.js
+++ liar-game/src/pages/SettingGames.js
@@ -14,10 +14,10 @@ const title_options = [
 ];
 
 const setting_infos = {
-  type : '1',           // 0 : 그림맞추기 , 1 : 단어맞추기,
+  // type : '1',           // 0 : 그림맞추기 , 1 : 단어맞추기,
   people : 3,           // 인원 수
   title : '',           // 게임 주제 : movie, cupNoodle, cookie, iceCream , food , fruit , exercise, singer, actor , title_song
-  info : [],            // 주제어,
+  info : "",            // 주제어,
   spy : false           // true : spy , false : spy X
 }
 
@@ -32,30 +32,23 @@ const setting_infos = {
 class SettingGames extends Component {
   state = {
     selectedOption_people: null,
-    // selectedOption_title: null,
-
+    movieList : []
 
   };
 
   handleChange = selectedOption_people => {
     this.httpGet("movie","20191215");
     console.log("selectedOption_people >>> " , selectedOption_people);
-      // this.setState(
-      //   { selectedOption_people },
-      //   () => console.log(`Option selected:`, this.state.selectedOption_people)
-      // );
-
-    // if (e.option === "title") {
-    //   this.setState(
-    //     // { selectedOption_title },
-    //     () => console.log(`Option selected:`, this.state.selectedOption_title)
-    //   );
-    // } else {
-    // }
+      this.setState(
+        { selectedOption_people },
+        () => console.log(`Option selected:`, this.state.selectedOption_people)
+      );
   };
 
   httpGet(type , addInfo) {
-    
+
+    const that = this;
+
     const request = require('request');
     let CLIENT_KEY = "";
     let aInfo = addInfo;
@@ -67,6 +60,9 @@ class SettingGames extends Component {
           url : `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key=${CLIENT_KEY}&targetDt=${aInfo}&weekGb=0`,
           type : 'get',
         }, function(error,res, body){
+          // if (body !== null  && body !== undefined) {
+          //   // that.setMovieList(body.boxOfficeResult.weeklyBoxOfficeList);
+          // }
           // console.log(res);
           console.log(body);
         });
@@ -75,6 +71,98 @@ class SettingGames extends Component {
       default:
         break;
     }
+  }
+
+  setMovieList (list) {
+
+    if (list.length > 0) {
+      list = list.filter((x) => list.movieNm);
+    }
+
+    console.log('list >>> ' , JSON.stringify(list));
+    // body
+
+  }
+
+  sendInfo () {
+    // setting_infos
+    this.setting_infos = {
+      // type : '1',           // 0 : 그림맞추기 , 1 : 단어맞추기,
+      people :this.state.selectedOption_people,           // 인원 수
+      title : '',           // 게임 주제 : movie, cupNoodle, cookie, iceCream , food , fruit , exercise, singer, actor , title_song
+      info : "",            // 주제어,
+      spy : false
+    }
+
+    console.log("setting_Infos >>> ", JSON.stringify(this.setting_infos));
+  }
+
+  render() {
+    const { selectedOption_people } = this.state;
+
+    // const { selectedOption_title } = this.state;
+
+    for (let i = 3; i< 21; i++) {
+      people_options.push({
+        'value' : i,
+        'label' : i + "명",
+        'option' : 'people'
+      });
+    }
+
+    return (
+
+      <div className="lg_container01">
+        <p className="lg_title01">Liar Games</p>
+        <div className="lg_btn01">
+          <button>그림 맞추기</button>
+          <button>단어 맞추기</button>
+        </div>
+        {/* <div className="lg_btn01">
+          <button>단어 맞추기</button>
+        </div> */}
+        <div className="lg_slectbox">
+          <Select readonly 
+            value={selectedOption_people}
+            name="people_number"
+            onChange={this.handleChange}
+            options={ people_options }
+            placeholder="인원 수"
+          />
+          <Select
+            placeholder="게임 주제"
+            onChange={this.handleChange}
+            options = {title_options}
+          />
+          {/* <Select
+            value={ selectedOption_title }
+            name="game_title"
+            onChange={ this.handleChange }
+            options={ title_options }
+            placeholder="게임 주제"
+          /> */}
+        </div>
+        {/* 스파이모드 선택 */}
+        <div className="lg_round">
+          <input type="checkbox" id="checkbox" />
+          <label htmlFor="checkbox">
+            <p>스파이모드</p>
+            <span className="lg_question"></span>
+          </label>
+        </div>
+        <div className="lg_btn02">
+          <button onClick={this.sendInfo}>시작하기</button>
+        </div>
+      </div>
+    );
+  }
+}
+export default SettingGames;
+
+// 선택 안된 경우 시작하기 disabled 처리
+// loading bar
+
+
 
 
 
@@ -217,67 +305,3 @@ class SettingGames extends Component {
 //     //  }
 //    });
 //  });
-
-  }
-
-  render() {
-    const { selectedOption_people } = this.state;
-
-    // const { selectedOption_title } = this.state;
-
-    for (let i = 3; i< 21; i++) {
-      people_options.push({
-        'value' : i,
-        'label' : i + "명",
-        'option' : 'people'
-      });
-    }
-
-    return (
-
-      <div className="lg_container01">
-        <p className="lg_title01">Liar Games</p>
-        <div className="lg_btn01">
-          <button>그림 맞추기</button>
-          <button>단어 맞추기</button>
-        </div>
-        {/* <div className="lg_btn01">
-          <button>단어 맞추기</button>
-        </div> */}
-        <div className="lg_slectbox">
-          <Select readonly 
-            value={selectedOption_people}
-            name="people_number"
-            onChange={this.handleChange}
-            options={ people_options }
-            placeholder="인원 수"
-          />
-          <Select
-            placeholder="게임 주제"
-            onChange={this.handleChange}
-            options = {title_options}
-          />
-          {/* <Select
-            value={ selectedOption_title }
-            name="game_title"
-            onChange={ this.handleChange }
-            options={ title_options }
-            placeholder="게임 주제"
-          /> */}
-        </div>
-        {/* 스파이모드 선택 */}
-        <div className="lg_round">
-          <input type="checkbox" id="checkbox" />
-          <label htmlFor="checkbox">
-            <p>스파이모드</p>
-            <span className="lg_question"></span>
-          </label>
-        </div>
-        <div className="lg_btn02">
-          <button>시작하기</button>
-        </div>
-      </div>
-    );
-  }
-}
-export default SettingGames;

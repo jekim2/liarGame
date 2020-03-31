cc748b25c17e1d31efd301372dc682ac143e0746
jekim2 <bluesara44>
2020-03-31 22:53:50 +0900

    asset모드 성공..

--- liar-game/src/App.js
+++ liar-game/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, HashRouter } from 'react-router-dom';
 import SettingGames from './pages/SettingGames';
 import SelectForm from './pages/SelectForm';
 import Success from './pages/Success';
@@ -8,16 +8,17 @@ import LoadingForm from './pages/LoadingForm';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div>
-        <Switch>
+        hi~!~!
+        {/* <Switch> */}
           <Route exact path="/" component={SettingGames} />
-          {/* <Route path="/selectform" component={SelectForm} />
+          <Route path="/selectform" component={SelectForm} />
           <Route path="/success" component={Success} />
-          <Route path="/loading" component={LoadingForm} /> */}
-        </Switch>
+          <Route path="/loading" component={LoadingForm} />
+        {/* </Switch> */}
       </div>
-    </BrowserRouter>
+    </HashRouter>
 
   );
 }
